1272b44ff55fa1d7f4e0cd96cff927ed14190562
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-07-19 13:59:41 +0900

    Title: 사각형 채우기, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240719/사각형 채우기/rectangle-fill.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 숫자 입력
+        int number = Integer.parseInt(br.readLine());
+
+        // DP 테이블 생성
+        int DP[] = new int[1001];
+
+        // 기본 설정
+        DP[1] = 1;
+        DP[2] = 2;
+
+        // 사각형 채우기
+        for(int i=3; i<1001; i++) {
+            DP[i] = (DP[i-1]+DP[i-2])%10007;
+        }
+
+        // 결과 출력
+        System.out.println(DP[number]);
+    }
+}
