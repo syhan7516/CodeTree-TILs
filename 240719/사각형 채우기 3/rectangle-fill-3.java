0e2f699282458737907130e901bd5365e6c75615
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-07-19 14:29:59 +0900

    Title: 사각형 채우기 3, Time: 110ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240719/사각형 채우기 3/rectangle-fill-3.java
+++ 240719/사각형 채우기 3/rectangle-fill-3.java
@@ -12,12 +12,16 @@ public class Main {
         int DP[] = new int[1001];
 
         // 기본 설정
+        DP[0] = 1;
         DP[1] = 2;
         DP[2] = 7;
 
         // 사각형 채우기
-        for(int i=3; i<1001; i++) {
-            DP[i] = (DP[i-1]+DP[i-2])*2% 1000000007;
+        for(int i=2; i<=number; i++) {
+            DP[i] = (DP[i-1]*2+DP[i-2]*3)%1000000007;
+            for(int j=i-3; j>=0; j--) {
+                DP[i] = (DP[i]+DP[j]*2)%1000000007;
+            }
         }
 
         // 결과 출력
