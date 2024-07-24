f329ce536f4e89d8915d410a9a37510727824713
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-07-24 10:16:29 +0900

    Title: 알파벳과 사칙연산, Time: 97ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240724/알파벳과 사칙연산/calculations-with-alphabet.java
+++ 240724/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -2,34 +2,76 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+
+    // 결과, 피연산자 개수
+    public static int answer, numCnt;
+
+    // 식
+    public static String letters;
+
+    // 선택된 피연산자 저장 리스트
+    public static ArrayList<Integer> selected;
+
+    // 피연산자 선택 메서드
+    public static void solve(int cnt) {
+
+        // 선택이 완료된 경우
+        if(cnt==numCnt) {
+
+            // 피연자 인덱스
+            int idx = 0; 
+
+            // 연산 결과
+            int result = selected.get(idx++);
+
+            // 연산자 확인
+            for(int i=1; i<letters.length(); i+=2) {
+                
+                // 현재 연산자
+                char c = letters.charAt(i);
+
+                // +
+                if(c=='+') result += selected.get(idx++);
+
+                // -
+                else if(c=='-') result -= selected.get(idx++);
+
+                // *
+                else result *= selected.get(idx++);
+            }
+
+            // 결과 갱신
+            answer = Math.max(answer,result);
+
+            return;
+        }
+
+        // 아닌 경우
+        for(int i=1; i<=4; i++) {
+            selected.add(i);
+            solve(cnt+1);
+            selected.remove(selected.size()-1);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         // 식 입력
-        String letters = br.readLine();
+        letters = br.readLine();
 
         // 결과
-        int answer = 4;
+        answer = 0;
 
-        // 식 확인
-        for(int i=1; i<letters.length(); i+=2) {
+        // 피연산자 개수
+        numCnt = letters.length()/2+1;
 
-            // 현재 문자
-            char c = letters.charAt(i);
+        // 선택된 피연산자 저장 리스트 생성
+        selected = new ArrayList<>();
 
-            // -
-            if(c=='-') answer -= 1;
-
-            // +
-            else if(c=='+') answer += 4;
-
-            // *
-            else {
-                if(answer<0) answer *= 1;
-                else answer *= 4;
-            }
-        }
-
+        // 피연산자 선택
+        solve(0);
+        
         // 결과 출력
         System.out.println(answer);
     }
