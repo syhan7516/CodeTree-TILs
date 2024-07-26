d7093583925cb0531c88769f2bf21824e79fbf32
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-07-26 18:37:19 +0900

    Title: 정수 사각형 최소 합, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240726/정수 사각형 최소 합/minimum-sum-path-in-square.java
+++ 240726/정수 사각형 최소 합/minimum-sum-path-in-square.java
@@ -10,34 +10,32 @@ public class Main {
         int size = Integer.parseInt(br.readLine());
 
         // 격자 배열 생성
-        int map[][] = new int[size+1][size+1];
+        int map[][] = new int[size][size];
 
         // 최소 합 배열 생성
-        int DP[][] = new int[size+1][size+1];
+        int DP[][] = new int[size][size];
 
         // 격자 정보 입력
-        for(int i=1; i<=size; i++) {
+        for(int i=0; i<size; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<size; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int i=0; i<=size; i++) {
-            for(int j=0; j<=size; j++) {
-                if(i==0 || j==0) DP[i][j] = Integer.MAX_VALUE;
-            }
-        }
+        // 초기 설정
+        DP[0][size-1] = map[0][size-1];
+        for(int i=size-2; i>=0; i--) DP[0][i] = DP[0][i+1]+map[0][i];
+        for(int i=1; i<size; i++) DP[i][size-1] = DP[i-1][size-1]+map[i][size-1];
 
         // 최소 합 구하기
-        for(int i=1; i<=size; i++) {
-            for(int j=size-1; j>=0; j--) {
-                if(i==size-1 && j==size-1) DP[i][j] = map[i][j];
-                else DP[i][j] = Math.min(DP[i-1][j],DP[i][j+1])+map[i][j];
+        for(int i=1; i<size; i++) {
+            for(int j=size-2; j>=0; j--) {
+                DP[i][j] = Math.min(DP[i-1][j],DP[i][j+1])+map[i][j];
             }
         }
-        
+         
         // 결과 출력
-        System.out.println(DP[size][0]);
+        System.out.println(DP[size-1][0]);
     }
 }
