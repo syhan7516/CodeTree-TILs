41d8d41c1e3a9311959bdc090ee167dac08bc99f
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-08-05 16:04:56 +0900

    Title: 외판원 순회, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240805/외판원 순회/traveling-salesman-problem.java
+++ 240805/외판원 순회/traveling-salesman-problem.java
@@ -1,13 +1,24 @@
 import java.util.*;
 import java.io.*;
 
+// 간선 클래스
+class Edge {
+    int city;
+    int dist;
+
+    public Edge(int city, int dist) {
+        this.city = city;
+        this.dist = dist;
+    }
+}
+
 public class Main {
 
     // 결과, 격자 크기
     public static int answer, size;
 
-    // 격자 배열
-    public static int map[][];
+    // 연결 관계 리스트
+    public static ArrayList<ArrayList<Edge>> relation;
 
     // 도시 방문 여부 배열
     public static boolean visited[];
@@ -15,8 +26,8 @@ public class Main {
     // 도시 순회하기 메서드
     public static void solve(int cnt, int city, int sum) {
 
-        // 이미 기존보다 경로가 더 긴 경우
-        if(sum>answer) return;
+        // 이미 기존보다 경로가 같거나 더 긴 경우
+        if(sum>=answer) return;
 
         // 모든 도시를 순회한 경우
         if(cnt==size) {
@@ -25,16 +36,18 @@ public class Main {
         }
 
         // 연결된 도시 확인
-        for(int i=0; i<size; i++) {
+        for(int i=0; i<relation.get(city).size(); i++) {
             
-            // 연결이 되어있으면서 미방문인 도시인 경우
-            if(map[city][i]>0 && !visited[i]) {
+            // 연결 도시
+            Edge current = relation.get(city).get(i);
 
-                // 도시 방문
-                visited[i] = true;
-                solve(cnt+1,i,sum+map[city][i]);
-                visited[i] = false;
-            }
+            // 이미 방문한 도시인 경우
+            if(visited[i]) continue;
+
+            // 도시 선택
+            visited[current.city] = true;
+            solve(cnt+1,current.city,sum+current.dist);
+            visited[current.city] = false;
         }
     }
 
@@ -45,19 +58,25 @@ public class Main {
         // 격자 크기 입력
         size = Integer.parseInt(br.readLine());
 
-        // 격자 배열 생성
-        map = new int[size][size];
+        // 연결 정보 리스트 생성
+        relation = new ArrayList<>();
 
-        // 격자 정보 입력
+        // 연결 정보 리스트 초기화
+        for(int i=0; i<=size; i++)
+            relation.add(new ArrayList<>());
+
+        // 연결 정보 입력
         for(int i=0; i<size; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<size; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                int dist = Integer.parseInt(st.nextToken());
+                if(dist==0) continue;
+                relation.get(i).add(new Edge(j,dist));
             }
         }
 
         // 도시 방문 여부 배열 생성
-        visited = new boolean[size];
+        visited = new boolean[size+1];
 
         // 도시 순회하기
         answer = Integer.MAX_VALUE;
