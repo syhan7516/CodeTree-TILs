b048c848856c96829277cd046fe0bd126b0b8d9d
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-09-26 16:04:48 +0900

    Title: Carry 피하기, Time: 3006ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

--- 240926/Carry 피하기/escaping-carry.java
+++ 240926/Carry 피하기/escaping-carry.java
@@ -12,22 +12,20 @@ public class Main {
     // carry 확인 메서드
     public static boolean carry(int a, int b) {
 
-        // 숫자 -> 문자열
-        String num1 = String.format("%09d",a);
-        String num2 = String.format("%09d",b); 
-
         // 확인
-        for(int i=num1.length()-1; i>=0; i--) {
-            
-            // 한 자리 수 가져오기
-            a = num1.charAt(i)-'0';
-            b = num2.charAt(i)-'0';
+        while(true) {
 
-            // 더한 후 확인
-            if((a+b)>9) return true;
+            // 둘 중 하나라도 0인 경우
+            if(a==0 || b==0) return false;
+
+            // 수 확인
+            int sum = (a%10)+(b%10);
+            if(sum>9) return true;
+
+            // 자리 변경
+            a = a/10;
+            b = b/10;
         }
-
-        return false;
     }
 
     // 숫자 선택 메서드
