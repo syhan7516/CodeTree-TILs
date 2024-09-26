7a36c8331dc2b080609d802941a825f2cf275be4
syhan7516 <83218200+syhan7516@users.noreply.github.com>
2024-09-26 16:11:28 +0900

    Title: Carry 피하기, Time: 99ms, Memory: 9MB, Status: Passed - CodeTree

--- 240926/Carry 피하기/escaping-carry.java
+++ 240926/Carry 피하기/escaping-carry.java
@@ -29,7 +29,7 @@ public class Main {
     }
 
     // 숫자 선택 메서드
-    public static void solve(int idx, int cnt, int selectedCnt, int sum) {
+    public static void solve(int cnt, int selectedCnt, int sum) {
 
         // 이미 불가능한 경우
         if((numCnt-cnt+selectedCnt)<=answer) return;
@@ -41,14 +41,11 @@ public class Main {
         }
 
         // 숫자 선택하기
-        for(int i=idx; i<numCnt; i++) {
+        solve(cnt+1,selectedCnt,sum);
 
-            solve(i+1,cnt+1,selectedCnt,sum);
-
-            // carry 확인
-            if(carry(sum,nums[i])) continue;
-            else solve(i+1,cnt+1,selectedCnt+1,sum+nums[i]);
-        }
+        // carry 확인
+        if(carry(sum,nums[cnt])) return;
+        else solve(cnt+1,selectedCnt+1,sum+nums[cnt]);
     }
 
     public static void main(String[] args) throws IOException {
@@ -66,7 +63,7 @@ public class Main {
 
         // 숫자 선택하기
         answer = 0;
-        solve(0,0,0,0);
+        solve(0,0,0);
 
         // 결과 출력
         System.out.println(answer);
